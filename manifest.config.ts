4ebfa5d19f5b3e383fc42693f15b3d22a9168cb8
Lasse Nielsen <lasse@xerrion.dk>
2026-01-27 11:36:12 +0100

    chore: update version numbers

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,7 +5,7 @@ export default defineManifest({
   name: "Auto Price Converter",
   description:
     "Automatically converts prices on websites to your chosen currency using live exchange rates",
-  version: "1.0.0",
+  version: "1.1.2",
   icons: {
     "16": "src/icons/icon16.png",
     "32": "src/icons/icon32.png",
